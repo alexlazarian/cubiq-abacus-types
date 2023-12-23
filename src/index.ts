f9a14f19137c4aab0606cf614b0d2da1e62224c4
Alex L <34932547+alexlazarian@users.noreply.github.com>
2023-12-23 15:27:10 -0800

    Add repository URL and update keywords and author in package.json Add student and group types in index.ts

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+// Student Types
+export interface IStudentGet {
+	payload: {}
+	response: {
+		name: string
+		email: string
+		avatar: string
+		password: string
+		groupObjectId: {
+			groupName: string
+			groupId: string
+		}
+		stars: number
+		joinedOn: string
+	}
+}
+
+export interface IStudentPost {
+	payload: {
+		name: string
+		email: string
+		groupId: string
+		password: string
+		avatar: string
+	}
+	response: {
+		student: IStudentGet['response']
+	}
+}
+
+export interface IStudentPut {
+	payload: {
+		name: string
+		email: string
+		groupId: string
+		password: string
+		avatar: string
+	}
+	response: {
+		student: IStudentGet['response']
+	}
+}
+
+export interface IStudentDelete {
+	payload: {
+		email: string
+	}
+	response: {
+		student: {
+			email: string
+		}
+	}
+}
+
+// Group Types
+
+export interface IGroupGet {
+	payload: {}
+	response: {
+		groupName: string
+		studentIds: string[]
+		groupId: string
+	}
+}
+
+// Error Types / Response Types
+
+export interface ErrorResponse {
+	message: string
+}
