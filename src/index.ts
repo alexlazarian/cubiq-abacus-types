3c18ba4c79a3aa294792b3036f88e12d332219cf
Alex L <34932547+alexlazarian@users.noreply.github.com>
2023-12-26 20:41:24 -0800

    add prettier and IGroupPost

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,90 @@
 // Student Types
 export interface IStudentGet {
-	payload: {}
-	response: {
-		name: string
-		email: string
-		avatar: string
-		password: string
-		groupObjectId: {
-			groupName: string
-			groupId: string
-		}
-		stars: number
-		joinedOn: string | Date
-	}
+    payload: {};
+    response: {
+        name: string;
+        email: string;
+        avatar: string;
+        password: string;
+        groupObjectId: {
+            groupName: string;
+            groupId: string;
+        };
+        stars: number;
+        joinedOn: string | Date;
+    };
 }
 
 export interface IStudentPost {
-	payload: {
-		name: string
-		email: string
-		groupId: string
-		password: string
-		avatar: string
-	}
-	response: {
-		student: IStudentGet['response']
-	}
+    payload: {
+        name: string;
+        email: string;
+        groupId: string;
+        password: string;
+        avatar: string;
+    };
+    response: {
+        student: IStudentGet['response'];
+    };
 }
 
 export interface IStudentPut {
-	payload: {
-		name: string
-		email: string
-		groupId: string
-		password: string
-		avatar: string
-	}
-	response: {
-		student: IStudentGet['response']
-	}
+    payload: {
+        name: string;
+        email: string;
+        groupId: string;
+        password: string;
+        avatar: string;
+    };
+    response: {
+        student: IStudentGet['response'];
+    };
 }
 
 export interface IStudentDelete {
-	payload: {
-		email: string
-	}
-	response: {
-		student: {
-			email: string
-		}
-	}
+    payload: {
+        email: string;
+    };
+    response: {
+        student: {
+            email: string;
+        };
+    };
 }
 
 // Group Types
 
 export interface IGroupGet {
-	payload: {}
-	response: {
-		groupName: string
-		studentIds: {
-			name: string
-			email: string
-			avatar: string
-			stars: number
-			joinedOn: Date | string
-		}[]
-		groupId: string
-	}
+    payload: {};
+    response: {
+        groupName: string;
+        studentIds: {
+            name: string;
+            email: string;
+            avatar: string;
+            stars: number;
+            joinedOn: Date | string;
+        }[];
+        groupId: string;
+    };
+}
+
+export interface IGroupPost {
+    payload: {
+        groupName: string;
+        studentEmails: Array<string>;
+    };
+    response: {
+        group: {
+            groupId: string;
+            groupName: string;
+            studentIds: Array<string>;
+        };
+    };
 }
 
 // Error Types / Response Types
 
 export interface ErrorResponse {
-	message: string
+    message: string;
 }
