69338e914047171b3ce8258bcf7c591aa0c9e12e
Alex L <34932547+alexlazarian@users.noreply.github.com>
2024-03-02 11:37:39 -0800

    change interface type key and add gameDigits

--- src/service/homeworkTypes.ts
+++ src/service/homeworkTypes.ts
@@ -26,13 +26,14 @@ export interface IHomeworkGet {
 export interface IHomeworkPost {
     payload: {
         assignTo: string;
-        startDate: string;
-        endDate: string;
+        dateEnd: string;
+        dateStart: string;
+        flashIn?: number;
+        flashOut?: number;
+        gameCategory: GameCategory;
+        gameDigits: number;
+        gameRounds: number;
         gameType: GameType;
-        category: GameCategory;
-        numberOfRounds: number;
-        appearInterval?: number;
-        disappearInterval?: number;
     };
     response: {
         homework: IHomeworkGet['response'];
