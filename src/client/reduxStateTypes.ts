89e8a76c7c0cb6870cdd0e67b97093e3e5612813
Alex L <34932547+alexlazarian@users.noreply.github.com>
2024-03-05 08:14:35 -0800

    Refactor reduxStateTypes.ts to include new interfaces for AssignmentsGroupedByDay

--- src/client/reduxStateTypes.ts
+++ src/client/reduxStateTypes.ts
@@ -1,6 +1,6 @@
 import { IAssignmentGet } from '../service/assignmentTypes';
 import { IGroupGet } from '../service/groupTypes';
-import { HomeworkStatus, IHomeworkGet } from '../service/homeworkTypes';
+import { HomeworkStatus } from '../service/homeworkTypes';
 import { IStudentGet } from '../service/studentTypes';
 
 export type IStudentsRedux = IStudentGet['response'] & {};
@@ -14,8 +14,38 @@ export type IHomeworksRedux = {
     endDateYYYYMMDD: string;
     expiresIn: string;
     status: HomeworkStatus;
-    assignments: IHomeworkGet['response']['assignmentIds'];
+    assignments: AssignmentsGroupedByDay;
 };
+
+interface AssignmentBank {
+    question: string;
+    correctAnswer: number;
+    userAnswer: number | null;
+    _id: string;
+}
+
+interface StudentObject {
+    name: string;
+    email: string;
+    avatar: string;
+}
+
+interface Assignment {
+    homeworkObjectId: string;
+    studentObjectId: StudentObject;
+    startOn: string;
+    endOn: string;
+    createdOn: string;
+    bank: AssignmentBank[];
+    status: 'completed' | 'pending'; // Assuming these are the only possible statuses
+    completedOn: string | null;
+    completedTime: number | null;
+}
+
+interface AssignmentsGroupedByDay {
+    [key: string]: Assignment[];
+}
+
 export type IAssignmentsRedux = IAssignmentGet['response'] & {
     due: string;
     daysDiff: number;
