8baf0a88df7033d5cd3da00deeb8ce6324ce35bc
Alex L <34932547+alexlazarian@users.noreply.github.com>
2024-03-10 12:25:25 -0700

    Fix bug in login functionality

---
+++ src/client/teacher/homeworks.ts
@@ -0,0 +1,45 @@
+import { GameStatus, HomeworkStatus } from '../../service/homeworkTypes';
+
+interface ITeacherHomeworkItemAssignment {
+    status: GameStatus;
+    homeworkId: string;
+    student: {
+        avatar: string;
+        correctAnswers: string;
+        email: string;
+        name: string;
+    };
+    dates: {
+        completedOn: string | null;
+        createdOn: string;
+        endOn: string;
+        startOn: string;
+    };
+    completedTime: number | null;
+    bank: {
+        isCorrect: boolean;
+        question: string;
+    }[];
+}
+interface AssignmentsGroupedByDay {
+    [key: string]: {
+        items: ITeacherHomeworkItemAssignment[];
+        submissions: string;
+        date: string;
+    };
+}
+
+export interface IClientTeacherHomeworks {
+    id: string;
+    groupName: string;
+    isEndDatePast: boolean;
+    startDateRaw: string | Date;
+    startDate: string;
+    startDateYYYYMMDD: string;
+    endDateRaw: string | Date;
+    endDate: string;
+    endDateYYYYMMDD: string;
+    expiresIn: string;
+    status: HomeworkStatus;
+    assignments: AssignmentsGroupedByDay;
+}
