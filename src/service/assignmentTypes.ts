d5e958985387c377c80ea57f168d86ce1cc4c69b
Alex L <34932547+alexlazarian@users.noreply.github.com>
2024-03-13 13:02:58 -0700

    Refactor assignmentTypes.ts to include new interfaces and update IAssignmentGet structure

--- src/service/assignmentTypes.ts
+++ src/service/assignmentTypes.ts
@@ -3,19 +3,40 @@
 import { GameStatus, IHomeworkGet } from './homeworkTypes';
 import { IStudentGet } from './studentTypes';
 
+interface IGameSettings {
+    mode: string;
+    category: string;
+    rounds: number;
+    chain: number;
+    flashIn: null;
+}
+
+interface IHomeworkObject {
+    gameSettings: IGameSettings;
+    _id: string;
+    dateStart: string;
+    dateEnd: string;
+    dateCreated: string;
+    reviewed: boolean;
+}
+
+interface IBankItem {
+    question: string;
+    correctAnswer: number;
+    userAnswer: null;
+    _id: string;
+}
+
 export interface IAssignmentGet {
     payload: {};
     response: {
-        studentObjectId: Pick<IStudentGet['response'], 'name' | 'email' | 'avatar'>;
-        bank: { question: string; correctAnswer: number; userAnswer: number | null; _id: string }[];
-        createdOn: Date;
-        endOn: Date;
-        homeworkObjectId: Omit<IHomeworkGet['response'], 'assignmentIds' | 'groupObjectId'>;
-        startOn: Date;
-        status: GameStatus;
-        completedOn: Date | null;
-        completedTime: number | null;
         _id: string;
+        homeworkObjectId: IHomeworkObject;
+        startOn: string;
+        endOn: string;
+        createdOn: string;
+        bank: IBankItem[];
+        status: string;
     };
 }
 
